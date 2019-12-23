1134a3aac2d21ffe799191779be19debfd785957
rosySerrano <59039924+rosySerrano@users.noreply.github.com>
2019-12-23 00:05:32 -0600

    Update guardarRegistro.php
    Se agrega código para guardar en archivo los datos del formulario de registro

--- guardarRegistro.php
+++ guardarRegistro.php
@@ -1,8 +1,28 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Descarga Libros PDF</title>
+    </head>
+    <body>
+        Guardar registro en archivo
+        <?php
+            //Ruta del archivo para registros
+            $path = "./recursos/datosRegistro.txt";
+        
+            // Variables para cada campo del formulario de registro
+            $nombre = filter_input(INPUT_POST, "name_control");
+            $userName = filter_input(INPUT_POST, "userName_control");
+            $password = filter_input(INPUT_POST, "password_control");
+            $email = filter_input(INPUT_POST, "email_control"); 
+            
+            //Generamos una cadena con los datos del formulario seperados por coma y al final de cada registro un punto y coma
+            $dato = $nombre.",".$userName.",".$password.",".$email.";";
+            
+            //Abrimos el archivo de registros para guardar el registro
+            $fileUsuarios = fopen($path, "a");
+            fwrite($fileUsuarios, $dato);
+            fclose($fileUsuarios);
+            
+        ?>
+    </body>
+</html>
