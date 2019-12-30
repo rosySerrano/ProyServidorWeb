24ab257555c8828665ff987b3fd6dc32de49caae
rosySerrano <59039924+rosySerrano@users.noreply.github.com>
2019-12-30 00:21:51 -0600

    Creacion Pie de pagina
    Se crea la leyenda de pie de pagina

---
+++ layout_foot.php
@@ -0,0 +1,18 @@
+<!-- Pie de página -->
+<footer class="page-footer font-small blue">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3"> 
+
+        Computación en el Servidor Web: Trabajo - Desarrollo Web Avanzado
+    </div>
+    <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+</div>
+
+<!-- Fin de la página HTML -->
+</body>
+</html>
