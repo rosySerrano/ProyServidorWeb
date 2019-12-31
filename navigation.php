5dca797580024e48df157eefd8166a9805d0592c
rosySerrano <59039924+rosySerrano@users.noreply.github.com>
2019-12-31 01:11:44 -0600

    Actualizacion
    Se modifica la navegación para presentar la opción de cierre de sesion.

--- navigation.php
+++ navigation.php
@@ -21,18 +21,15 @@ $namePage = array_pop($urlPage);
             <?php
             // comprueba si los usuarios / clientes iniciaron sesión
 // si el usuario inició sesión, muestre las opciones "Editar perfil" y "Cerrar sesión"
-            if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true && $_SESSION['access_level'] == 'Customer') {
+            if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true && trim($_SESSION['access_level']) == 'Lector') {
+                
+
                 ?>
-                <ul class="nav navbar-nav navbar-right">
-                    <li <?php echo $page_title == "Edit Profile" ? "class='active'" : ""; ?>>
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-                            &nbsp;&nbsp;<?php echo $_SESSION['firstname']; ?>
-                            &nbsp;&nbsp;<span class="caret"></span>
+                <ul class="nav navbar-nav navbar-right">                   
+                    <li <?php echo $namePage == $page_login ? "class='active'" : ""; ?>>
+                        <a href="<?php echo $home_url . $page_logout; ?>">
+                            <span class="glyphicon glyphicon-log-out"></span> Cerrar sesión
                         </a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="<?php echo $home_url; ?>logout.php">Cerrar sesion</a></li>
-                        </ul>
                     </li>
                 </ul>
                 <?php
@@ -42,7 +39,7 @@ $namePage = array_pop($urlPage);
             else {
                 ?>
                 <ul class="nav navbar-nav navbar-right">
-                   
+
                     <!--Presenta la opción "Login". Solo si la pagina actual es la pagina de inicio de sesión se presenta activa esta opción-->
                     <li <?php echo $namePage == $page_login ? "class='active'" : ""; ?>>
                         <a href="<?php echo $home_url . $page_login; ?>">
