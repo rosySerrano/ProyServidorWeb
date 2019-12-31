c63c6685f35fd1866d8132afc17162bba7d3da47
rosySerrano <59039924+rosySerrano@users.noreply.github.com>
2019-12-31 02:55:27 -0600

    Creación
    Se agrega la página que servira para los usuarios con perfil de admintradores. Solo esta la indicación de una funcionalidad

---
+++ admin.php
@@ -0,0 +1,19 @@
+<?php
+
+// configuración central
+include_once "config/core.php";
+
+// establecemos el título de la página
+$page_title = "Administración de Usuarios";
+
+// se incluye el verificador de inicio de sesión
+$require_login = true;
+include_once "login_checker.php";
+
+// incluimos el HTML de encabezado de página
+include_once "layout_head.php";
+
+echo "<img src='./recursos/SITIO-EN-CONSTRUCCION.jpg' class='img-thumbnail'  width='300' height='300'>";
+
+// pie de página 
+include_once "layout_foot.php";
