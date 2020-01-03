01898ab6fa01a6e16fbf26676f495fef405ee27f
rosySerrano <59039924+rosySerrano@users.noreply.github.com>
2020-01-02 23:56:19 -0600

    update login_checker.php
    Se cambia la sentencia de control while por if

--- login_checker.php
+++ login_checker.php
@@ -1,7 +1,7 @@
 <?php
 
 // validacion de si $ require_login fue establecido y el valor es 'verdadero'
-while(isset($require_login) && $require_login==true){
+if(isset($require_login) && $require_login==true){
 	// si el usuario aún no ha iniciado sesión, es redirigido a la página de inicio de sesión
 	if(!isset($_SESSION['access_level'])){
 		header("Location: {$home_url}login.php?action=please_login");
